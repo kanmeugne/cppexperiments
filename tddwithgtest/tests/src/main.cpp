df38dc32de69497fe5b0a0d1daf99e873960f7f6
Patrick SIMO KANMEUGNE <patrick.simo-kanmeugne@outlook.com>
2020-10-26 18:30:52 +0100

    remove redondant declarations

--- tddwithgtest/tests/src/main.cpp
+++ tddwithgtest/tests/src/main.cpp
@@ -1,24 +1,18 @@
 #include <gmock/gmock.h>
-#include "Parser.h"
+#include "MyParser.h"
 
 using namespace testing;
 
-class ParserTest : public Test
-{
-public:
-	utils::Parser_t _parser;
+class ParserTest : public Test{};
 
-	void SetUp() override {
-		_parser = utils::getParser();
-	}
-};
-
-TEST_F(ParserTest, Parser_LowerSingleLetter)
+TEST(ParserTest, Parser_LowerSingleLetter)
 {
     std::string output = "";
     std::string input = "L";
-    _parser->convertToLowerCase(input, output)
-	ASSERT_EQ(output, Eq("l"));
+    std::string expected = "l";
+    utils::MyParser_t& parser = utils::getParser();
+    parser->convertToLowerCase(input, output);
+	ASSERT_EQ(output, "l");
 }
 
 int main(int argc, char** argv)
