abbbbd47770f1daa2600a1c710e428ef628529f3
Patrick S. Kanmeugne <patrick.simokanmeugne@outlook.com>
2020-10-28 18:31:41 +0100

    set empty files to get started with

--- tddwithgtest/tests/src/main.cpp
+++ tddwithgtest/tests/src/main.cpp
@@ -3,18 +3,6 @@
 
 using namespace testing;
 
-class ParserTest : public Test{};
-
-TEST(ParserTest, Parser_LowerSingleLetter)
-{
-    std::string output = "";
-    std::string input = "L";
-    std::string expected = "l";
-    utils::MyParser_t& parser = utils::getParser();
-    parser->convertToLowerCase(input, output);
-	ASSERT_EQ(output, "l");
-}
-
 int main(int argc, char** argv)
 {
     testing::InitGoogleMock(&argc, argv);
--- tddwithgtest/toolset/include/MyParser.h
+++ tddwithgtest/toolset/include/MyParser.h
@@ -1,19 +1,5 @@
 #ifndef MYPARSER_H
-#include <string>
-#include <memory>
 
-namespace utils
-{
-    class MyParser
-    {
-    public:
-        MyParser();
-        ~MyParser();
+class MyParser;
 
-        void convertToLowerCase(const std::string &, std::string &);
-        void convertToUpperCase(const std::string &, std::string &);
-    };
-    typedef std::unique_ptr<MyParser> MyParser_t;
-    MyParser_t &getParser();
-};     // namespace utils
 #endif // MYPARSER_H
